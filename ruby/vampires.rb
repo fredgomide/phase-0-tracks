c1713efdb98a5d81b51f49babee4e98a82d3538f
FSG <fredgomide@gmail.com>
2017-06-26 08:41:56 -0500

    Vampire program done with allergies loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,34 +7,47 @@ until process_times == 0
   puts "Next employee"
   process_times -= 1
 
-## Program inputs ##
-puts "What is your name?"
+  ## Program inputs ##
+  puts "What is your name?"
     name = gets.chomp
 
-puts "How old are you? What year were you born?"
-    employee_age = gets.chomp.to_i
-    year_born = gets.chomp.to_i
-     age = 2017 - year_born
-    if age == employee_age
-       age = true
-      else
-       age = false
+  puts "How old are you? What year were you born?"
+      employee_age = gets.chomp.to_i
+      year_born = gets.chomp.to_i
+       age = 2017 - year_born
+      if age == employee_age
+         age = true
+        else
+         age = false
+      end
+
+  puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+      garlic_bread = gets.chomp
+      if garlic_bread == "y"
+      garlic_bread = true
+        else
+      garlic_bread = false
     end
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-    garlic_bread = gets.chomp
-    if garlic_bread == "y"
-    garlic_bread = true
-      else
-    garlic_bread = false
-  end
+  puts "Would you like to enrole in the company's health insurance?"
+      health_insurance = gets.chomp
+      if health_insurance == "y"
+        health_insurance = true
+        else
+        health_insurance = false
+      end
 
-puts "Would you like to enrole in the company's health insurance?"
-    health_insurance = gets.chomp
-    if health_insurance == "y"
-      health_insurance = true
+  puts "Name any allergies you might have and type 'done' when finished."
+     allergies = gets.chomp
+    until allergies == "sunshine" do
+       puts "Do you have any other allergies?"
+       allergies = gets.chomp
+            if allergies == "sunshine"
+        allergies = true
       else
-      health_insurance = false
+        allergies = false
+    end
+       break if allergies == "done"
     end
 
     ## logic ##
@@ -51,16 +64,17 @@ puts "Would you like to enrole in the company's health insurance?"
 
     #scenario 3 - Probably a vampire#
     #If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
-      elsif (age && garlic_bread) or health_insurance
+      elsif (age && garlic_bread) || health_insurance || allergies
     puts "Probably a vampire"
 
     #scenario 4 - Almost certainly a vampire#
     #If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
-      elsif (age || garlic_bread) and !health_insurance
+      elsif (age || garlic_bread) && !health_insurance
     puts "Almost certainly a vampire"
 
       else
       puts "Result Inconclusive"
     end
-  end
 
+    puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+  end
