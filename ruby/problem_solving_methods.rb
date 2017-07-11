bfdbbf60a1358b71cdbf806d132860d289f7dd11
FSG <fredgomide@gmail.com>
2017-07-11 11:15:21 -0500

    Dont know how to solve

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,9 +1,9 @@
 
-def search_array
-arr.each do |i|
-  puts i
-end
+def search_array(arr, i)
+  arr = []
+  arr.each[i]
+
 end
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-p search_array(1)
+p search_array(arr, 1)
